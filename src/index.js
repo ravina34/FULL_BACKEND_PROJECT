e8f911ffb8562ae266e52a6af22ccae7ff9acbd7
ravina34 <krabina514@gmail.com>
2026-02-18 16:09:53 +0530

    "n"

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ dotenv.config({
 })
 
  connectDB()
+ .then(()=>
+{
+    app.listen(process.env,PORT || 8000, ()=>
+    {
+        console.log(` server is running  on port:  ${process.env.PORT}`)
+    }) 
+})
+ .catch((err) => {
+    console.log("mongo db connection failed !!!", err);
+ })
 
 
 
